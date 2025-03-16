5ced088a0c7aa651b6b5a817a215717326e5273b
Haviv Rosh <haviv_rosh@Havivs-MacBook-Pro-2.local>
2025-03-16 20:56:54 +0200

    Added params.txt file and new parameter documentation

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/hello', methods=['GET'])
 def hello():
-    return jsonify({"message": "Hello, World!"})
+    return jsonify({"message": "Hello, World v2!"})
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5001) 
